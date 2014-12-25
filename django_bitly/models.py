1531e8cc4127b046b9f8e411de44a0bcccf78aa5
Alex Morozov <*inductor2000@mail.ru*>
2014-12-25 13:16:09 +0400

    Django 1.7 compatibility fix.

--- django_bitly/models.py
+++ django_bitly/models.py
@@ -12,7 +12,7 @@ from django.contrib.sites.models import Site
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.conf import settings
-from django.utils import simplejson as json
+import json
 
 from .conf import BITLY_TIMEOUT
 from .exceptions import BittleException
